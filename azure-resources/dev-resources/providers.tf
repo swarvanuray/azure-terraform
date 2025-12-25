927ebb61b9ca6fe51e3f341ec0ec7514c88bacef
Swarvanu Ray <swarvanuray@gmail.com>
2025-12-25 14:37:13 -0600

    Configure OIDC authentication

--- azure-resources/dev-resources/providers.tf
+++ azure-resources/dev-resources/providers.tf
@@ -11,12 +11,14 @@ terraform {
     storage_account_name = "tfaccount1"
     container_name       = "state-file"
     key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
   subscription_id = "764444e2-3113-461c-b2ca-7bb5df60fb41"
+  use_oidc        = true
 
   # ADD THIS LINE TO FIX THE ERROR:
   resource_provider_registrations = "none"
